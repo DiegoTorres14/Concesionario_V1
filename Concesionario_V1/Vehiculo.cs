bf8b564643ea9d5ae02a241c5a1bbdf471ba3218
Diego Torres Contreras <dietoco92@hotmail.com>
2024-01-29 12:37:12 +0100

    Clase Vehiculo: Miembros privados, propiedades  y constructores

---
+++ Concesionario_V1/Vehiculo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario_V1
+{
+    public class Vehiculo
+    {
+        // CONSTANTES
+
+        // MIEMBROS PRIVADOS
+        private string _marca;
+        private string _modelo;
+        // CONSTRUCTORES
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        // PROPIEDADES
+        public string Marca
+        {
+            get
+            { 
+                return _marca;
+            }
+            set
+            {
+                // TODO: Proporcionar seguridad y protección al miembro - _marca
+                _marca = value;
+            }
+        }
+
+        public string Modelo
+        {
+            get
+            {
+                return _modelo;
+            }
+            set
+            {
+                // TODO: Proporcionar seguridad y protección al miembro - _modelo
+                _modelo = value;
+            }
+        }
+
+        // MÉTODOS PÚBLICOS
+
+        // MÉTODOS PRIVADOS
+        
+    }
+}
