a1b482af202989def3abda1d41472c956dd0726a
Diego Torres Contreras <dietoco92@hotmail.com>
2024-01-29 12:06:55 +0100

    Agregar archivos de proyecto.

---
+++ Concesionario_V1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Concesionario_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            // MENÚ DE ACCIONES A REALIZAR
+
+            // PROCESAMIENTO DE LAS ACCIONES
+        }
+    }
+}
